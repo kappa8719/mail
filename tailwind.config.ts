a72ee9ed545bacb741b7ab85405fab984583b183
Kappa <kappa@entropi.kr>
2025-05-07 01:32:37 +0900

    feat: add fastInSlowOut easing (source Android)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,9 @@ export default {
   theme: {
   	extend: {
 		transitionTimingFunction: {
+			fastInSlowOut: 'cubic-bezier(0.4, 0.0, 0.2, 1.0)',
 			primary: 'cubic-bezier(0.32, 0.72, 0, 1)',
+			// primary: 'cubic-bezier(0.4, 0.0, 0.2, 1.0)',
 		},
   		colors: {
   			background: 'hsl(var(--background))',
