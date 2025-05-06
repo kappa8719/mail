2c3f4ebaa7f4f757e727ca0f33ba32a6438b99c2
Kappa <kappa@entropi.kr>
2025-05-07 01:30:56 +0900

    fix: optimize imports

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,24 +1,10 @@
-import { Button } from '@/components/ui/button'
-import {
-  AlertCircle,
-  Archive,
-  Inbox,
-  Mail,
-  Send,
-  SettingsIcon,
-  Star,
-  Tag,
-  Trash2,
-} from 'lucide-react'
-import { ScrollArea } from '@/components/ui/scroll-area'
-import { Separator } from '@/components/ui/separator'
 import { ReactNode } from 'react'
 import { ViewTransitions } from 'next-view-transitions'
 import { Sidebar } from '@/components/sidebar/sidebar'
 
 export default async function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex h-dvh w-dvw bg-gradient-to-b from-background to-background/95 overflow-hidden">
+    <div className="flex h-dvh w-dvw overflow-hidden bg-gradient-to-b from-background to-background/95">
       <Sidebar />
 
       <div className="z-0 w-full">
